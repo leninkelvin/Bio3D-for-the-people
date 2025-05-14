22bd180473fde940b2e944814fd032aa45e872a7
leninkelvin <24532230+leninkelvin@users.noreply.github.com>
2025-05-14 14:06:33 -0600

    Update ProteinPCA-specificv2.0.R
    Added DT library for html table, notes for annotations an colors

--- ProteinPCA-specificv2.0.R
+++ ProteinPCA-specificv2.0.R
@@ -2,36 +2,61 @@ library(bio3d)
 library(httr)
 library(dplyr)
 library(openxlsx)
+library(DT)
+
 
 # Download some example PDB files
-ids <- c('1a28',
-         '1e3k',
-         '1sqn',
-         '1sr7',
-         '1zuc',
-         '2c7a',
-         '2ovh',
-         '2ovm',
-         '2w8y',
-         '3d90',
-         '3g8o',
-         '3hq5',
-         '3kba',
-         '3zr7',
-         '3zra',
-         '3zrb',
-         '4a2j',
-         '4apu',
-         '4oar',
-         '5cc0'
-         )
+
+{ids <- c('1a28',
+          '1e3k',
+          '1sqn',
+          '1sr7',
+          '1zuc',
+          '2c7a',
+          '2ovh',
+          '2ovm',
+          '2w8y',
+          '3d90',
+          '3g8o',
+          '3hq5',
+          '3kba',
+          '3zr7',
+          '3zra',
+          '3zrb',
+          '4a2j',
+          '4apu',
+          '4oar',
+          '5cc0'
+)} # All PDB
+
+ids <- c('1a28_A',
+         '1e3k_A',
+         '1sqn_A',
+         '1sr7_A',
+         '1zuc_A',
+         '2ovh_A',
+         '2ovm_A',
+         '2w8y_A',
+         '3d90_A',
+         '3g8o_A',
+         '3hq5_A',
+         '3kba_A',
+         '3zr7_A',
+         '3zra_A',
+         '3zrb_A',
+         '4a2j_A',
+         '4apu_A',
+         '4oar_A'
+         ) # Selected PDBs and chains
+
 raw.files <- get.pdb(ids)
 
 anno <- pdb.annotate(ids)
-View(anno)
+datatable(anno)
 
-write.xlsx(anno, 'anno.xlsx')
+write.xlsx(anno, 'anno.xlsx') # Annotations are manually introduced. Some are resolution, coregulator and ligand type
 annotation <- cbind(anno, color)
+datatable(annotation)
 
 # Extract and align the chains we are interested in
 files <- pdbsplit(raw.files, ids)
@@ -71,12 +96,12 @@ hist(rd, breaks=40, xlab="RMSD (Å)", main="Histogram of RMSD")
 hc.rd <- hclust(as.dist(rd))
 
 pdbs$id <- substr(basename(pdbs$id), 1, 6)
-hclustplot(hc.rd, k = 4, colors=annotation[, "color"], labels=pdbs$id, cex=1.2,
+hclustplot(hc.rd, k = 4, colors=annotation[, "coregulator"], labels=pdbs$id, cex=1.2,
            ylab="RMSD (Å)", main="RMSD Cluster Dendrogram", fillbox=FALSE)
 
 	# Save as PNG
 		png("RMSD_cluster.png", width=1200, height=800)
-		hclustplot(hc.rd, k = 4, colors=annotation[, "color"], labels=pdbs$id, cex=1.2, 
+		hclustplot(hc.rd, k = 4, colors=annotation[, "coregulator"], labels=pdbs$id, cex=1.2, 
 		ylab="RMSD (Å)", main="RMSD Cluster Dendrogram", fillbox=FALSE)
     dev.off()
 
@@ -88,30 +113,32 @@ gaps.pos <- gap.inspect(pdbs$xyz)
 pc.xray <- pca.xyz(xyz[, gaps.pos$f.inds])
 pc.xray
 
-plot(pc.xray, col=annotation[, "color"])
+plot(pc.xray, col=annotation[, "coregulator"])
 
     # Save as PNG
     png("PCAx4.png", width=1200, height=800)
-    plot(pc.xray, col=annotation[, "color"])
+    plot(pc.xray, col=annotation[, "coregulator"])
     dev.off()
 
 hc <- hclust(dist(pc.xray$z[,1:2]))
 grps <- cutree(hc, h=30)
 cols <- c("red", "green", "blue")[grps]
-plot(pc.xray, pc.axes=1:2, col=annotation[, "color"])
+plot(pc.xray, pc.axes=1:2, col=annotation[, "coregulator"])
 
       # Save as PNG
       png("PCAcluster.png", width=1200, height=800)
-      plot(pc.xray, pc.axes=1:2, col=annotation[, "color"])
+      plot(pc.xray, pc.axes=1:2, col=annotation[, "coregulator"])
       dev.off()
 
 # Dendrogram plot
 names(cols) <- pdbs$id
-hclustplot(hc, col=annotation[, "color"], ylab="Distance in PC Space", main="PC1-2", cex=0.9)
+hclustplot(hc, col=annotation[, "coregulator"], ylab="Distance in PC Space", main="PC1-2", cex=0.9)
 
       # Save as PNG
       png("PCA_dendogram.png", width=1200, height=800)
-      hclustplot(hc, col=annotation[, "color"], ylab="Distance in PC Space", main="PC1-2", cex=0.9)
+      hclustplot(hc, col=annotation[, "coregulator"], ylab="Distance in PC Space", main="PC1-2", cex=0.9)
       dev.off()
+
+
 
 
